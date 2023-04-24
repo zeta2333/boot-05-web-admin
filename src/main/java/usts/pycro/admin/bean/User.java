cb9de86d2462776ad402fdd03bcc259de4736a23
Pycro <2023747015@qq.com>
2023-04-24 10:58:03 +0800

    4.24 commit

---
+++ src/main/java/usts/pycro/admin/bean/User.java
@@ -0,0 +1,14 @@
+package usts.pycro.admin.bean;
+
+import lombok.Data;
+
+/**
+ * @author Pycro
+ * @version 1.0
+ * 2023-04-24 10:57 AM
+ */
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
